f969af26d33f2f514a3406ec1f5e79c87a0101ce
Mathew Daugherty <mathewdaugherty@mac.com>
2024-11-19 15:10:15 -0500

    Finished more sections

---
+++ 19/19-assignment-1/src/App.vue
@@ -0,0 +1,86 @@
+<template>
+  <div>
+  <h2>My Course Goal</h2>
+  <!-- Task 1: Output your main course goal with help of the composition API -->
+  <!-- Don't hardcode it into the template, instead hardcode it into the JS code -->
+  <h3 v-if="isVisible">{{ goal }}</h3>
+  <!-- Task 2: Toggle (show/ hide) the goal with help of the button  -->
+  <button @click="toggleGoal">Toggle Goal</button>
+  <!-- Task 3: Manage data in three ways -->
+  <!-- => Separate refs -->
+  <!-- => Ref Object -->
+  <!-- => Reactive Object -->
+  <!-- Task 4: Also solve the assignment with the Options API -->
+</div>
+</template>
+
+<script>
+// import {  reactive } from 'vue'; // ref,
+
+export default {
+
+  // setup() {
+
+  //   // const courseGoal = "Finish the course and learn Vue!";
+  //   // const goalIsVisible = ref(false);
+    
+  //   // const courseData = ref({
+  //   //   goal: "Finish the course and learn Vue!",
+  //   //   isVisible: false,
+  //   // });
+
+  //   // function toggleGoal() {
+  //   //   // goalIsVisible.value = !goalIsVisible.value;
+  //   //   courseData.value.isVisible = !courseData.value.isVisible;
+  //   // }
+
+  //   // return {
+  //   //   goal: courseData.value.goal,
+  //   //   courseData,
+  //   //   toggleGoal,
+  //   // };
+
+  //   const courseData = reactive({
+  //     goal: "Finish the course and learn Vue!",
+  //     isVisible: false,
+  //   });
+
+  //   function toggleGoal() {
+  //     courseData.isVisible = !courseData.isVisible;
+  //   }
+
+  //   return {
+  //     goal: courseData.goal,
+  //     courseData,
+  //     toggleGoal,
+  //   };
+
+
+  // }
+
+  data() {
+    return {
+      goal: "Finish the course and learn Vue!",
+      isVisible: false,
+    };
+  },
+  methods: {
+    toggleGoal() {
+      this.isVisible = !this.isVisible;
+    },
+  },
+  
+
+  
+}
+</script>
+
+<style>
+html {
+  font-family: sans-serif;
+}
+body {
+  margin: 3rem;
+  text-align: center;
+}
+</style>
---
+++ 19/19-composition-api/src/components/UserData.vue
@@ -0,0 +1,65 @@
+<template>
+  <div>
+    <h2>{{ userName }}</h2>
+    <h3>{{ age }}</h3>
+  </div>
+</template>
+
+<script>
+import {
+  computed,
+  inject,
+  onBeforeMount,
+  onMounted,
+  onBeforeUpdate,
+  onUpdated,
+  onBeforeUnmount,
+  onUnmounted,
+} from 'vue';
+
+export default {
+  props: ['firstName', 'lastName'],
+  // computed: {
+  //     userName() {
+  //         return this.firstName + ' ' + this.lastName;
+  //     },
+  // },
+  setup(props, context) {
+    const uName = computed(function () {
+      return props.firstName + ' ' + props.lastName;
+    });
+
+    const age = inject('userAge');
+
+    console.log(context);
+
+    onBeforeMount(() => {
+      console.log('Before Mount');
+    });
+
+    onMounted(() => {
+      console.log('Mounted');
+    });
+
+    onBeforeUpdate(() => {
+      console.log('Before Update');
+    });
+
+    onUpdated(() => {
+      console.log('Updated');
+    });
+
+    onBeforeUnmount(() => {
+      console.log('Before Unmount');
+    });
+
+    onUnmounted(() => {
+      console.log('Unmounted');
+    });
+
+    
+
+    return { userName: uName, age };
+  },
+};
+</script>
